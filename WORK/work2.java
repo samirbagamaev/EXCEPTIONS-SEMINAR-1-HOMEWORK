cfcbcf15fce408250beb63d33e98ba87770df2ce
Samir Bagamaev <05ru4315@gmail.com>
2023-08-27 01:19:27 +0300

    EXCEPTIONS SEMINAR 1 HOMEWORK

---
+++ WORK/work2.java
@@ -0,0 +1,76 @@
+/*
+Реализуйте метод subArrays, принимающий в качестве аргументов два целочисленных массива a и b, и возвращающий новый массив c, каждый элемент которого равен разности элементов двух входящих массивов в той же ячейке.
+
+Если длины массивов не равны - верните нулевой массив длины 1.
+
+Напишите свой код в методе subArrays класса Answer. Метод subArrays принимает на вход два параметра:
+
+int[] a - первый массив
+int[] b - второй массив
+Пример
+
+
+a = new int[]{4, 5, 6};
+b = new int[]{1, 2, 3};
+
+Вывод: [3, 3, 3]
+
+a = new int[]{4, 5, 6};
+b = new int[]{1, 2, 3, 5};
+
+Вывод: [0]
+*/
+
+package WORK;
+
+public class work2 {
+    public int[] subArrays(int[] a, int[] b){
+        if(a == null || b == null || a.length != b.length){
+            return new int[1]; // нулевой массив длины 1 будет означать различие в длине массивов
+        }
+        else{
+            int[] c = new int[a.length];
+            for(int i = 0; i < a.length; i++){
+                c[i] = a[i] - b[i];
+            }
+            return c;
+        }
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+
+class Answer {
+    public int[] subArrays(int[] a, int[] b){
+        if(a == null || b == null || a.length != b.length){
+            return new int[1]; // нулевой массив длины 1 будет означать различие в длине массивов
+        }
+        else{
+            int[] c = new int[a.length];
+            for(int i = 0; i < a.length; i++){
+                c[i] = a[i] - b[i];
+            }
+            return c;
+        }
+    }
+}
+
+public class Printer{ 
+    public static void main(String[] args) { 
+      int[] a = {};
+      int[] b = {};
+
+      if (args.length == 0) {
+        a = new int[]{4, 5, 6};
+        b = new int[]{1, 2, 3};
+      }
+      else{
+        a = Arrays.stream(args[0].split(", ")).mapToInt(Integer::parseInt).toArray();
+        b = Arrays.stream(args[1].split(", ")).mapToInt(Integer::parseInt).toArray();
+      }     
+
+      Answer ans = new Answer(); 
+      String itresume_res = Arrays.toString(ans.subArrays(a, b));     
+      System.out.println(itresume_res);
+    }
+}
